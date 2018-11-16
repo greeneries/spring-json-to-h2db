0af317e47511f4181426bae35c1a3c8dab1d6c9f
greenery <elindreams@hanmail.net>
2018-11-16 19:16:07 +0900

    .

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+
+insert into cooking_type (cooking_type) values ('끊이기'); 
+insert into cooking_type (cooking_type) values ('굽기'); 
+insert into cooking_type (cooking_type) values ('찌기'); 
+insert into cooking_type (cooking_type) values ('기타'); 
+insert into cooking_type (cooking_type) values ('튀기기'); 
+insert into cooking_type (cooking_type) values ('볶기'); 
+
+insert into food_type (food_type) values ('밥'); 
+insert into food_type (food_type) values ('국&찌개'); 
+insert into food_type (food_type) values ('반찬'); 
+insert into food_type (food_type) values ('일품'); 
+insert into food_type (food_type) values ('후식'); 
+
+
